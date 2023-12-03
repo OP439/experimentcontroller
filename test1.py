1a137eef446509b92f9f6ed394ad23b9a3d78d56
Owain <56586315+OP439@users.noreply.github.com>
2023-12-03 10:26:14 +0000

    fix to not have the distractor job3

--- test1.py
+++ test1.py
@@ -28,10 +28,10 @@ def job2():
     time.sleep(1)
 
 
-def job3():
-    time.sleep(1)
-    print(requests.get('https://lab1.owainpill.de/autotimer?timetonext='+str(schedule.idle_seconds())))
-    time.sleep(1)
+# def job3():
+#     time.sleep(1)
+#     print(requests.get('https://lab1.owainpill.de/autotimer?timetonext='+str(schedule.idle_seconds())))
+#     time.sleep(1)
 
         
 
@@ -45,10 +45,11 @@ schedule.every(22).minutes.do(job1)#job to rotate printer and then trigger takin
 schedule.every(20).to(35).minutes.do(job2)
 
 # keep the website updated with when commands are scheduled to be run from the script
-schedule.every(2).minutes.do(job3)
+#schedule.every(2).minutes.do(job3) you always get this as next of course
 
 while 1:
     schedule.run_pending()
     print(str(schedule.idle_seconds())+' time to next')
+    print(requests.get('https://lab1.owainpill.de/autotimer?timetonext='+str(schedule.idle_seconds())))
     time.sleep(1)
 
